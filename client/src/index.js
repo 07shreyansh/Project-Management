575723b046d6643f4e88c5f15c8b145e99ac99d5
07shreyansh <shriyanshlohiya2002@gmail.com>
2024-06-06 03:24:52 +0530

    react router dom error resolved

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ConfigProvider } from "antd";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <ConfigProvider
